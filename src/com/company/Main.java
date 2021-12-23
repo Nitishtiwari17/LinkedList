2bcb578a80e09104bcb91231bcf9e9add2c3460c
Nitish Kumar Tiwari <nitishtiwari1710@gmail.com>
2021-12-23 19:29:43 +0530

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+import java.util.LinkedList;
+public class Main {
+
+    public static void main(String[] args) {
+        LinkedList<String> l_list=new LinkedList<String>();
+        l_list.add("Red");
+        l_list.add("Green");
+        l_list.add("white");
+        l_list.add("Pink");
+        l_list.add("Yellow");
+
+        System.out.println("The LinkedList list:"+l_list);
+	// write your code here
+    }
+}
